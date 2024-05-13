b797ed5442e5e206e363a0dec451b8466e526503
itapurnama06 <127809157+itapurnama06@users.noreply.github.com>
2024-05-13 12:15:39 +0800

    Add files via upload

---
+++ Pertemuan9.java
@@ -0,0 +1,20 @@
+package pertemuan9;
+
+/**
+ *
+ * @author LAB F
+ */
+public class Pertemuan9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+           //new frm01().setVisible(true);
+        frm01 f1 = new frm01();
+        f1.setResizable(false);
+        f1.setAlwaysOnTop(true);
+        f1.setVisible(true);
+    }
+    
+}
